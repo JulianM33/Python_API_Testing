b3f2100baaaadbd5caed7bf83a58e61418543831
Julian MK <julian.meijerink@gmail.com>
2022-07-14 17:47:01 +0200

    Add languages test

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import requests
 import json
 
+API_KEY = "54bb0787a703d69941a9db676179294f"
+
 url_top_movies = "https://developers.themoviedb.org/3/movies/get-top-rated-movies"
 url_rate_movie = "https://developers.themoviedb.org/3/movies/rate-movie"
-url_auth = "https://api.themoviedb.org/3/movie/550?api_key=54bb0787a703d69941a9db676179294f"
+url_auth = "https://api.themoviedb.org/3/movie/550?api_key={}".format(API_KEY)
 
-API_KEY = "54bb0787a703d69941a9db676179294f"
+url_language = "https://api.themoviedb.org/3/movie/76341?api_key={}&language=".format(API_KEY)
+
+iso_codes = ["ab", "aa", "af", "ak", "sq", "am", "ar", "an", "hy", "as", "av", "ae", "ay", "az", "bm", "ba", "eu",
+             "be", "bn", "bi", "bs", "br", "bg", "my", "ca", "ch", "ce", "ny", "zh", "cu", "cv", "kw", "co", "cr",
+             "hr", "cs", "da", "dv", "nl", "dz", "en", "eo", "et", "ee", "fo", "fj", "fi", "fr", "fy", "ff", "gd",
+             "gl", "lg", "ka", "de", "el", "kl", "gn", "gu", "ht", "ha", "he", "hz", "hi", "ho", "hu", "is", "io",
+             "ig", "id", "ia", "ie", "iu", "ik", "ga", "it", "ja", "jv", "kn", "kr", "ks", "kk", "km", "ki", "rw",
+             "ky", "kv", "kg", "ko", "kj", "ku", "lo", "la", "lv", "li", "ln", "lt", "lu", "lb", "mk", "mg", "ms",
+             "ml", "mt", "gv", "mi", "mr", "mh", "mn", "na", "nv", "nd", "nr", "ng", "ne", "no", "nb", "nn", "ii",
+             "oc", "oj", "or", "om", "os", "pi", "ps", "fa", "pl", "pt", "pa", "qu", "ro", "rm", "rn", "ru", "se",
+             "sm", "sg", "sa", "sc", "sr", "sn", "sd", "si", "sk", "sl", "so", "st", "es", "su", "sw", "ss", "sv",
+             "tl", "ty", "tg", "ta", "tt", "te", "th", "bo", "ti", "to", "ts", "tn", "tr", "tk", "tw", "ug", "uk",
+             "ur", "uz", "ve", "vi", "vo", "wa", "cy", "wo", "xh", "yi", "yo", "za", "zu"]
 
 
 def test_basic():
@@ -24,7 +38,11 @@ def test_auth():
     # # str = json.loads(response.json())
     response.json()
 
-    assert response.status_code == 205320
+    assert response.status_code == 200
 
 
-
+def test_language_codes():
+    for iso_code in iso_codes:
+        request = url_language + iso_code
+        response = requests.get(request)
+        assert response.status_code == 200
