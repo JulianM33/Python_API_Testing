693b8edb105ab0a3c30d35dbca4e401b13d2bdec
Julian MK <julian.meijerink@gmail.com>
2022-07-14 17:17:49 +0200

    Add auth test

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import requests
+import json
 
 url_top_movies = "https://developers.themoviedb.org/3/movies/get-top-rated-movies"
 url_rate_movie = "https://developers.themoviedb.org/3/movies/rate-movie"
+url_auth = "https://api.themoviedb.org/3/movie/550?api_key=54bb0787a703d69941a9db676179294f"
+
+API_KEY = "54bb0787a703d69941a9db676179294f"
 
 
-def test_answer():
+def test_basic():
     response = requests.get(url_top_movies)
     print(response)
 
     print("content = ", response.content)
     print("headers = ", response.headers)
 
-    assert(response.status_code == 200)
+    assert response.status_code == 200
+
+
+def test_auth():
+    response = requests.get(url_auth)
+    # print("response = ", response)
+    # # str = json.loads(response.json())
+    response.json()
+
+    assert response.status_code == 205320
+
+
+
