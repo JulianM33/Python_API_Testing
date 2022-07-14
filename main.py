8a2e597f5d4e1534c085886d6ccd54023d8fa9f0
Julian MK <julian.meijerink@gmail.com>
2022-07-15 01:51:25 +0200

    Add 404 and 403 test, added comments

--- main.py
+++ main.py
@@ -13,8 +13,12 @@ url_language = "https://api.themoviedb.org/3/movie/76341?api_key={}&language=".f
 url_post = "https://api.themoviedb.org/3/account/favorite?api_key={}&session_id=3".format(API_KEY)
 url_review = "https://api.themoviedb.org/3/movie/278/reviews?api_key={}&language=en-US&page=1".format(API_KEY)
 
+url_403 = "https://www.themoviedb.org/random/page/error"
+url_404 = "https://api.themoviedb.org/3/movie/2131513/images?api_key=54bb0787a703d69941a9db676179294f"
 
 
+
+# Test basic GET API
 def test_basic():
     response = requests.get(url_top_movies)
     print(response)
@@ -25,76 +29,63 @@ def test_basic():
     assert response.status_code == 200
 
 
+# Test simple usage of the given Auth_API key
 def test_auth():
     response = requests.get(url_auth)
-    # print("response = ", response)
-    # # str = json.loads(response.json())
-    # response.json()
-
     assert response.status_code == 200
 
 
+# Test bad request
+def test_403():
+    response = requests.get(url_403)
+    print(response)
+    assert response.status_code == 403
+
+
+# Test not found request
+def test_404():
+    response = requests.get(url_404)
+    print(response)
+    assert response.status_code == 404
+
+
+# Test API returning an image
 def test_img():
     response = requests.get(url_img)
-    print("headers ", response.headers)
-
     assert response.status_code == 200
     assert response.headers['Content-Type'] == "image/png"
 
 
-def test_review():
-    response = requests.get(url_img)
-    print("headers ", response.headers)
-
-    assert response.status_code == 200
-    assert response.headers['Content-Type'] == "image/png"
-
-
-def test_body_post_favorite():
-    response = requests.post("https://api.themoviedb.org/3/movie/278/rating?api_key=54bb0787a703d69941a9db676179294f",
-                             # data={"value": 8.5},
-                             data="{\"value\": 8.5}",
-                             headers={'Authorization': 'Bearer {}'.format(TOKEN)}
-                             )
-    print(response)
-    print(response.text)
-
-
-# c8315bec7e32adcfd35e0abb75f55f9362d761de
 def test_get_token():
+    # Begin by obtaining initial auth token
     url_get_token = "https://api.themoviedb.org/3/authentication/token/new?api_key={}".format(API_KEY)
     response = requests.get(url_get_token)
     print(response)
-    # print(response.text)
+
+    assert response.status_code == 200
+
+    # Store token obtained given in the JSON response
     NEW_TOKEN = response.json()["request_token"]
-    # NEW_TOKEN = "4e94d7348015b159140e9ca35556f9ed469a4656"
     print(NEW_TOKEN)
     print()
 
+    # Approving token requires manual user approval, we print link so that we can authenticate
+    # In order to prevent proceeding before the user approval, we can run in debug mode and put a
+    #  breakpoint before the last  requests.post() call in line 89
     url_authenticate = "https://www.themoviedb.org/authenticate/{}".format(NEW_TOKEN)
     print(url_authenticate)
 
-    # response = requests.get(url_authenticate)
-    # print(response)
-    # print(response.headers)
-    # print()
-
+    # Make the request body needed for obtained the session_id
     request_body = "\"request_token\": \"{token}\"".format(token=NEW_TOKEN)
     request_body = "{" + request_body + "}"
     print(request_body)
 
-    url_authenticate = "https://api.themoviedb.org/3/authentication/session/new?api_key={}".format(API_KEY)
-    response = requests.post(url_authenticate,
+    # This should be the URL at which we obtain the session_id, but link is not working
+    # When trying the same POST request in Postman, the same result is still obtained
+    url_session = "https://api.themoviedb.org/3/authentication/session/new?api_key={}".format(API_KEY)
+    response = requests.post(url_session,
                              data=request_body
                              )
     print(url_authenticate)
     print(response)
     print(response.text)
-
-
-test_get_token()
-# def test_language_codes():
-#     for iso_code in iso_codes:
-#         request = url_language + iso_code
-#         response = requests.get(request)
-#         assert response.status_code == 200
