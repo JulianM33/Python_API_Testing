e6721d2b3c5a21c469d1dc7e986436e338c08a3a
Julian MK <julian.meijerink@gmail.com>
2022-07-14 16:19:13 +0200

    Add first basic Pytest

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 import requests
 
-
 url_top_movies = "https://developers.themoviedb.org/3/movies/get-top-rated-movies"
 url_rate_movie = "https://developers.themoviedb.org/3/movies/rate-movie"
 
-response = requests.get(url_top_movies)
-print(response)
 
+def test_answer():
+    response = requests.get(url_top_movies)
+    print(response)
+
+    print("content = ", response.content)
+    print("headers = ", response.headers)
+
+    assert(response.status_code == 200)
